9ce6031505e5a9fa0620927da5079396cef64a3a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-07 12:41:29 +1200

    flask app update

--- routes.py
+++ routes.py
@@ -5,10 +5,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return("This is the homepage")
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return "This is the about page"
+
+@app.route("/EDP")
+def edp():
+    connection = sqlite3.connect('fragrance.db')
+    cur = connection.cursor()
+    cur.execute("SELECT bottle_name, bottle_description, bottle_concentration FROM Fragrance WHERE bottle_concentration = 'EDP'")
+    fragrance_edp = cur.fetchall()
+    return render_template('pizza.html', fragrance_edp = fragrance_edp)
 
 
+    
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
