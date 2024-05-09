9e8b34afdbc72208f80e24b34f71a126b05840e4
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-09 15:19:20 +1200

    flask app and html templates update

--- routes.py
+++ routes.py
@@ -12,11 +12,15 @@ def about():
     return "This is the about page"
 
 
-#Route for EDP Page 
+#Route for EDP page 
 @app.route("/EDP")
 def edp():
     connection = sqlite3.connect('fragrance.db') #connect to database 
     cur = connection.cursor()
+
+    cur.execute("SELECT bottle_concentration FROM Fragrance where bottle_concentration = 'EDP'")
+    fragrance_concentration = cur.fetchall()
+
     cur.execute("SELECT bottle_name FROM Fragrance WHERE bottle_concentration = 'EDP'") #select bottle name
     fragrance_name = cur.fetchall()
 
@@ -26,7 +30,29 @@ def edp():
     cur.execute("SELECT bottle_description FROM Fragrance WHERE bottle_concentration = 'EDP'")
     fragrance_desc = cur.fetchall() #select bottle description
 
-    return render_template('fragrance_info.html', name = fragrance_name, brand = fragrance_brand, description = fragrance_desc) #display info with templates
+    return render_template('fragrance_info.html', concentration = fragrance_concentration, name = fragrance_name, brand = fragrance_brand, description = fragrance_desc) #display info with templates
+
+
+
+
+#Route for EDT page
+@app.route("/EDT")
+def edt():
+    connection = sqlite3.connect('fragrance.db') #connect to database 
+    cur = connection.cursor()
+    cur.execute("SELECT bottle_name FROM Fragrance WHERE bottle_concentration = 'EDT'") #select bottle name
+    fragrance_name = cur.fetchall()
+
+    cur.execute("SELECT bottle_concentration FROM Fragrance where bottle_concentration = 'EDT'")
+    fragrance_concentration = cur.fetchall()
+
+    cur.execute("SELECT brand_name from Fragrance INNER JOIN Designer ON Fragrance.bottle_brand = Designer.brand_id WHERE bottle_concentration = 'EDT'")
+    fragrance_brand = cur.fetchall() #select bottle brand
+
+    cur.execute("SELECT bottle_description FROM Fragrance WHERE bottle_concentration = 'EDT'")
+    fragrance_desc = cur.fetchall() #select bottle description
+
+    return render_template('fragrance_info.html', concentration = fragrance_concentration, name = fragrance_name, brand = fragrance_brand, description = fragrance_desc) #display info with templates
 
 
  
